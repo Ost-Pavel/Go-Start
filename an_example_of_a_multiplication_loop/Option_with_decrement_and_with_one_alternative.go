d3dec10b4d621b938a0f008c3df1c26f519deb45
Ost_Pavel <70640327+Ost-Pavel@users.noreply.github.com>
2021-09-21 12:01:22 +0300

    Option with decrement and with one variable
    Вариант с декрементом и с одной переменной (помимо счетчика).

--- an_example_of_a_multiplication_loop/Option_with_decrement_and_with_one_alternative.go
+++ an_example_of_a_multiplication_loop/Option_with_decrement_and_with_one_alternative.go
@@ -1,14 +1,12 @@
 package main
-
 import "fmt"
-
 func main() {
+
     var input int
     fmt.Scanln(&input)
     
     for i := input-1; i > 0; i-- {        
         input *= i
     }  
-    
-    fmt.Println(input)
+        fmt.Println(input)
 }
